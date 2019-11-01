9e0ae110c3a06eaf55c073892bfcb86c2e4a8593
vasilii <porokh.vs@gmail.com>
2019-11-01 03:33:05 +0300

    add module

--- src/index.js
+++ src/index.js
@@ -1,34 +1,24 @@
+
+
 import DATA from './data'
 import Background from './Backround/Background'
 import initUiChangerAnimations from './ui'
 import NewsWiget from './NewsBlock/Wiget'
 
-Background(document.querySelector('#canvas-wrapper'))
+Background( document.querySelector('#canvas-wrapper') )
 
 const newsWiget = new NewsWiget()
-newsWiget.appendTo(document.querySelector('#canvas-wrapper2'))
+newsWiget.appendTo( document.querySelector('#canvas-wrapper2') )
 newsWiget.createNewsBlocks( DATA )
-window.addEventListener( 'resize', () => {
-    newsWiget.resize()
-}, false )
 
+window.addEventListener( 'resize', () => newsWiget.resize(), false )
 initUiChangerAnimations({
-    'One': () => {
-        newsWiget.playScenario('One')
-    },
-    'Two': () => {
-        newsWiget.playScenario('Two')
-    },
-    'Three': () => {
-        newsWiget.playScenario('Three')
-    },   
+    'One': () => newsWiget.playScenario( 'One' ),
+    'Two': () => newsWiget.playScenario( 'Two' ),
+    'Three': () => newsWiget.playScenario( 'Three' ),   
 })  
 
 newsWiget.playScenario( 'One' )
-setTimeout( () => { 
-    newsWiget.stop()
-}, 2000 ) 
-setTimeout( () => { 
-    newsWiget.delete()
-}, 4000 ) 
+setTimeout( () => newsWiget.stop(), 2000 ) 
+setTimeout( () => newsWiget.delete(), 4000 ) 
 
